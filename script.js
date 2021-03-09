866f070c72f1acbe91e4bbe0aa54ff9935e724ef
Morten Six <mort108v@stud.kea.dk>
2021-03-09 20:48:28 +0100

    Cap product images shown in preview working

--- script.js
+++ script.js
@@ -2,70 +2,82 @@
 
 // The model of all features
 const features = {
-  drinksholder: false,
-  led: false,
-  propeller: false,
-  shield: false,
-  solarfan: false
+    drinksholder: false,
+    led: false,
+    propeller: false,
+    shield: false,
+    solarfan: false
 };
 
 window.addEventListener("DOMContentLoaded", start);
 
 function start() {
-  console.log("start");
-  // register toggle-clicks
-  document.querySelectorAll(".option").forEach(option => option.addEventListener("click", toggleOption));
+    console.log("start");
+    // register toggle-clicks
+    document.querySelectorAll(".option").forEach(option => option.addEventListener("click", toggleOption));
 }
 
 function toggleOption(event) {
-  const target = event.currentTarget;
-  const feature = target.dataset.feature;
+    console.log(event);
+    const target = event.currentTarget;
+    const feature = target.dataset.feature;
 
-  // TODO: Toggle feature in "model"
+    // TODO: Toggle feature in "model"
+    features[feature] = !features[feature];
 
-  // If feature is (now) turned on:
-  // - mark target as chosen (add class "chosen")
-  // - un-hide the feature-layer(s) in the #product-preview;
-  // - create featureElement and append to #selected ul
-  // - create FLIP-animation to animate featureElement from img in target, to
-  //   its intended position. Do it with normal animation or transition class!
 
-  // Else - if the feature (became) turned off:
-  // - no longer mark target as chosen
-  // - hide the feature-layer(s) in the #product-preview
-  // - find the existing featureElement in #selected ul
-  // - create FLIP-animation to animate featureElement to img in target
-  // - when animation is complete, remove featureElement from the DOM
-  
-  if (features[feature]) {
-    // feature added
-    console.log(`Feature ${feature} is turned on!`);
+    // If feature is (now) turned on:
+    if (features[feature]) {
+        console.log("Add chosen");
+        // - mark target as chosen (add class "chosen")
+        target.classList.add("chosen");
+        // - un-hide the feature-layer(s) in the #product-preview;
+        const productDataFeatures = document.querySelectorAll(`img[data-feature=${feature}]`);
 
-    // TODO: More code
+        productDataFeatures.forEach(productDataFeature => {
+            productDataFeature.classList.remove("hide");
+        });
+    }
+    // - create featureElement and append to #selected ul
+    // - create FLIP-animation to animate featureElement from img in target, to
+    //   its intended position. Do it with normal animation or transition class!
 
-  } else {
-    // feature removed
-    console.log(`Feature ${feature} is turned off!`);
-    
-    // TODO: More code
+    // Else - if the feature (became) turned off:
+    // - no longer mark target as chosen
+    // - hide the feature-layer(s) in the #product-preview
+    // - find the existing featureElement in #selected ul
+    // - create FLIP-animation to animate featureElement to img in target
+    // - when animation is complete, remove featureElement from the DOM
 
-  }
+    if (features[feature]) {
+        // feature added
+        console.log(`Feature ${feature} is turned on!`);
+
+        // TODO: More code
+
+    } else {
+        // feature removed
+        console.log(`Feature ${feature} is turned off!`);
+
+        // TODO: More code
+
+    }
 }
 
 // Create featureElement to be appended to #selected ul - could have used a <template> instead
 function createFeatureElement(feature) {
-  const li = document.createElement("li");
-  li.dataset.feature = feature;
+    const li = document.createElement("li");
+    li.dataset.feature = feature;
 
-  const img = document.createElement("img");
-  img.src = `images/feature_${feature}.png`;
-  img.alt = capitalize(feature);
+    const img = document.createElement("img");
+    img.src = `images/feature_${feature}.png`;
+    img.alt = capitalize(feature);
 
-  li.append(img);
+    li.append(img);
 
-  return li;
+    return li;
 }
 
 function capitalize(text) {
-  return text.substring(0, 1).toUpperCase() + text.substring(1).toLowerCase();
+    return text.substring(0, 1).toUpperCase() + text.substring(1).toLowerCase();
 }
